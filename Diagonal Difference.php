220d9cbaf5751db02d19d0ec5e40747b8ae85373
orayb alsmadi <orayb.alsmadi@mawdoo3.com>
2020-02-13 16:49:23 +0200

    Diagonal difference

---
+++ Diagonal Difference.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference($arr) {
+    // Write your code here
+    $pri = 0;
+    $sec = 0;
+    for($i=0; $i<count($arr[0]); $i++){
+       $pri += $arr[$i][$i]; 
+       $sec += $arr[$i][count($arr[0])-$i-1];
+    }
+return abs($pri-$sec) ;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$arr = array();
+
+for ($i = 0; $i < $n; $i++) {
+    $arr_temp = rtrim(fgets(STDIN));
+
+    $arr[] = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+}
+
+$result = diagonalDifference($arr);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
