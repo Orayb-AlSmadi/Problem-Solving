08759dda1cb0e3451ef0e99107b932008e362052
orayb alsmadi <orayb.alsmadi@mawdoo3.com>
2020-02-16 12:08:45 +0200

    StairCase

---
+++ staircase.php
@@ -0,0 +1,24 @@
+<?php
+
+// Complete the staircase function below.
+function staircase($n) {
+
+$x=1;
+for ($j=$n; $j>0; $j--){
+echo str_repeat(" ", $j-1);
+
+for($i=0; $i<$x; $i++){
+    echo"#";
+}
+echo"\n";
+$x++;
+}
+}
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $n);
+
+staircase($n);
+
+fclose($stdin);
